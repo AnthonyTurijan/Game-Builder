feab7914e7777be2241a7084f7e902fd252d90bf
AnthonyTurijan <anthonyturijan@gmail.com>
2022-02-28 19:16:13 -0800

    Resolved Grid Layout with Placeholders

---
+++ index.js
@@ -0,0 +1,6 @@
+// let cards = document.getElementsByClassName("card-spread")
+// function cardOpacity(){
+//     let cards = document.getElementsByClassName("card-spread");
+//     cards.style.opacity = 0
+// }
+// cardOpacity()
