fd16204587e6eb9edd7a4e28f14c81a79131b2db
AnthonyTurijan <anthonyturijan@gmail.com>
2022-03-02 19:19:05 -0800

    Simple JS Code

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
 // forming an array to hold all images
-let card = document.getElementsByClassName('card-spread')
+let card = document.getElementsByClassName('card')
 let cards = [...card]
 
-//adding on click event listener
-function clickCards(){
-    for (let i = 0; i < cards.length; i++){
-        cards[i].addEventListener('click', function(){
-            
-        })
+//Fisher-Yates Shuffle Method
+function shuffleCards(array){
+    const cardDeck = array.length, temporaryValue, randomIndex;
+
+    while (cardDeck !== 0){
+        randomIndex = Math.floor(Math.random() * cardDeck);
+        cardDeck -=1;
+        temporaryValue = array[cardDeck];
+        array[randomIndex] = temporaryValue
     }
+    return array;
 }
+//Function for shuffling cards on new page
+document.body.onload = newGame();
+
+function newGame(){
+    cards = shuffleCards(cards)
+}
+
+//adding on click event listener
+// function clickCards(){
+//     for (let i = 0; i < cards.length; i++){
+//         cards[i].addEventListener('click', function(){
+//             document.body.style.color = 'black'
+//         })
+//     }
+// }
